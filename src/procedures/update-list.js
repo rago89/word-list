fe08fa3a334a8ead9f8c9ae1af0ba4b5ab5ef8eb
rafael garcia <rago89@gmail.com>
2021-05-26 15:54:20 +0200

    reorder planning folders and add demo.html to demo folder

--- src/procedures/update-list.js
+++ src/procedures/update-list.js
@@ -1,10 +1,11 @@
 const listContainer = document.getElementById('list-container');
 
 /**
- * Renders an array of strings as an unordered list into the #list-container element
+ * Renders an array of strings as an un ordered list into the #list-container element
  *
  * @param {string[]} [toRender=[]] - an array of strings to render into the UI
  */
+
 export const updateList = (toRender = []) => {
   const listItems = toRender.reduce((all, next) => {
     return `${all}<li>${next}</li>`;
