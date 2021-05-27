46d2a903dc742146dd9044ec2e902980503ab9df
rafael garcia <rago89@gmail.com>
2021-05-27 17:04:33 +0200

    add input-word, sort-word

--- src/handlers/input-word.js
+++ src/handlers/input-word.js
@@ -1,10 +1,10 @@
-import { data } from '../data.js';
-import { isWord } from '../logic/is-word.js';
-import { sortStrings } from '../logic/sort-strings.js';
-import { updateList } from '../procedures/update-list.js';
+import { data } from "../data.js";
+import { isWord } from "../logic/is-word.js";
+import { sortStrings } from "../logic/sort-strings.js";
+import { updateList } from "../procedures/update-list.js";
 
-const warnings = document.getElementById('warnings');
-
+// DOM elements
+const warnings = document.getElementById("warnings");
 /**
  * Entry point for users adding a word to the list.
  * It is called each time the user clicks the "add word" button.
@@ -14,37 +14,31 @@ const warnings = document.getElementById('warnings');
 export const handleInputWord = (event) => {
   /* -- entry point for adding or removing a word -- */
   // debugger;
-  console.log('-- handler: input word --');
+  console.log("-- handler: input word --");
 
   /* -- check the target -- */
-  if (event.target.type !== 'button') {
+  if (event.target.type !== "button") {
     return;
   }
-
   /* -- gather user input from DOM -- */
   const text = event.target.form.text.value;
   const action = event.target.value;
-
-  /* -- use the input and data to implement the user story --
-
-    a user can add a new word to the list
-      given the input contains non-letters,
-        it will not be added
-        a warning is displayed
-      given the input contains only letters
-        it will be added to the words list
-        the list will be re-rendered
-    a user can remove words from the list
-      given the input is not in the list
-        a warning is posted
-      given the input is in the list
-        it is removed
-        the list is re-rendered
-  */
-
-  // ... write some code ...
-
-  /* -- render new words -- */
+  if (action === "add") warnings.innerHTML = "";
+  // send an error if word don't contain only letters
+  if (action === "add") {
+    if (!isWord(text)) {
+      warnings.innerHTML = `Your input: "${text}" must contain only letters`;
+      return;
+    }
+    data.words.push(text);
+  }
+  if (action === "remove") {
+    if (!data.words.includes(text)) {
+      warnings.innerHTML = `"${text}" is not in the list`;
+      return;
+    }
+    data.words.splice(data.words.indexOf(text), 1);
+  }
   const sorted = sortStrings(data.words, data.sort);
   updateList(sorted);
 };
--- src/handlers/sort-words.js
+++ src/handlers/sort-words.js
@@ -1,6 +1,6 @@
-import { data } from '../data.js';
-import { sortStrings } from '../logic/sort-strings.js';
-import { updateList } from '../procedures/update-list.js';
+import { data } from "../data.js";
+import { sortStrings } from "../logic/sort-strings.js";
+import { updateList } from "../procedures/update-list.js";
 
 /**
  * Entry point for users sorting the list of words in this app.
@@ -9,21 +9,11 @@ import { updateList } from '../procedures/update-list.js';
  * @param {Event} event - The event triggered by changing the input.
  */
 export const handleSortWords = (event) => {
-  /* -- entry point for sorting the words -- */
-  // debugger;
-  console.log('-- handler: sort words --');
+  console.log("-- handler: sort words --");
 
   /* -- gather user input from DOM -- */
   const howToSort = event.target.value;
 
-  /* -- use the input and data to create a new sorted list --
-    change the .sort property in data
-    sort the words in data.words using the logic function
-    assign the newly sorted list to a variable named `sorted`
-  */
-
-  // ... write some code ...
-
-  /* -- render new words -- */
+  const sorted = sortStrings(data.words, howToSort);
   updateList(sorted);
 };
